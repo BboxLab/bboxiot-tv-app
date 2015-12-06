6006bb326f2f3ad7750c6a3dfbf209ff9f68826d
Bertrand Martel <kiruazoldik92@gmail.com>
2015-12-06 23:56:36 +0100

    add association event obj test

---
+++ app/src/androidTest/java/fr/bouyguestelecom/tv/bboxiotsample/test/AssociationEventObjTest.java
@@ -0,0 +1,37 @@
+package fr.bouyguestelecom.tv.bboxiotsample.test;
+
+import android.test.AndroidTestCase;
+
+import java.util.Date;
+
+import fr.bouyguestelecom.tv.bboxiotsample.AssociationEventObj;
+
+/**
+ * @author Bertrand Martel
+ */
+public class AssociationEventObjTest extends AndroidTestCase {
+
+    private AssociationEventObj eventObject = null;
+    private String deviceUid = "deviceUidTest";
+    private String eventStr = "eventTeset";
+
+    public AssociationEventObjTest() {
+        eventObject = new AssociationEventObj(deviceUid, eventStr);
+        assertNotNull("AssociationEventObj is null", eventObject);
+    }
+
+    public void getDateTest() {
+        assertNotNull("association event date is null", eventObject.getDate());
+        assertTrue("date is invalid (too old)", eventObject.getDate().getTime() >= (new Date().getTime()));
+    }
+
+    public void getDeviceUidTest() {
+        assertNotNull("deice uid is null", eventObject.getDeviceUid());
+        assertTrue("device uid invalid", eventObject.getDeviceUid().equals(deviceUid));
+    }
+
+    public void getEventStrTest() {
+        assertNotNull("event content is null", eventObject.getEventStr());
+        assertTrue("event content invalid", eventObject.getEventStr().equals(eventStr));
+    }
+}
